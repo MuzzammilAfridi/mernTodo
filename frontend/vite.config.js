86db7e3d851febdb54b7ab79b718ce0fa2c7c780
MuzzammilAfridi <118999159+MuzzammilAfridi@users.noreply.github.com>
2025-01-12 15:35:17 +0530

    again update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+
   build: {
     outDir: 'dist', // Ensure this matches your deployment settings
   },
